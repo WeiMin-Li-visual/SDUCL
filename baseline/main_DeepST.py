b0213570b5a361e034abc9ec85d68435788313a3
WeiMin-Li-visual <58018142+WeiMin-Li-visual@users.noreply.github.com>
2024-10-16 17:21:43 +0800

    Update main_DeepST.py

--- baseline/main_DeepST.py
+++ baseline/main_DeepST.py
@@ -12,7 +12,6 @@ from utils_func import calculate_clustering_matrix
 # warning
 warnings.filterwarnings("ignore")
 
-# 初始化参数
 data_path = "/data/STAGATE_pyG/Data/"
 save_path = "Results"
 
@@ -33,30 +32,20 @@ for data_name, n_domains in zip(dataset, n_clusters):
                  # choose the number of training
                  use_gpu=True)
 
-    # 读取数据
     adata = deepen._get_adata(platform="Visium", data_path=data_path, data_name=data_name)
-
-    # 分割组织学图像
     adata = deepen._get_image_crop(adata, data_name=data_name)
-
-    # 数据增强
     adata = deepen._get_augment(adata, spatial_type="LinearRegress", use_morphological=True)
 
-    # 构建图
     graph_dict = deepen._get_graph(adata.obsm["spatial"], distType="BallTree")
-
-    # PCA对数据进一步处理
     data = deepen._data_process(adata, pca_n_comps=200)
 
-    # 训练模型
     deepst_embed = deepen._fit(
         data=data,
         graph_dict=graph_dict, )
 
-    # DeepST得到的embedding
+
     adata.obsm["DeepST_embed"] = deepst_embed
 
-    # priori表示数据是否有先验知识（簇数）
     adata = deepen._get_cluster_data(adata, n_domains=n_domains, priori=True)
     path = os.path.join(data_path, data_name)
     df_meta = pd.read_csv(path + '/metadata.tsv', sep='\t')
@@ -64,7 +53,6 @@ for data_name, n_domains in zip(dataset, n_clusters):
     adata.obs['ground_truth'] = df_meta_layer.values
     adata = adata[~pd.isnull(adata.obs['ground_truth'])]
     
-    # ARI判断聚类结果
     ARI = metrics.adjusted_rand_score(adata.obs['DeepST_refine_domain'], adata.obs['ground_truth'])
     print(ARI)
     ARI = round(ARI, 3)
